94eb0a40540dc90a97feda0a2e4a199fe8eb4fd8
Aniruddh Mishra <aniruddhm17@gmail.com>
2023-07-11 16:07:42 -0500

    created a spot on the window for two keyboards; also added a way to change the keys on the keyboard

--- keyboard.py
+++ keyboard.py
@@ -1,19 +1,9 @@
 import customtkinter
 
-class MyCheckboxFrame(customtkinter.CTkFrame):
-    def __init__(self, master):
-        super().__init__(master)
-
-        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox 1")
-        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
-        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
-
 class keyboard(customtkinter.CTkFrame):
-    def __init__(self, master):
+    def __init__(self, master, characters):
         super().__init__(master)
         
-        alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
         for i in range(26):
             if i < 10:
                 row = 0
@@ -29,5 +19,5 @@ class keyboard(customtkinter.CTkFrame):
                 padx = (17, 3)
             if row == 2:
                 padx = (3, 17)
-            key = customtkinter.CTkLabel(self, text=alphabet[i], text_color="black")
+            key = customtkinter.CTkLabel(self, text=characters[i], text_color="black")
             key.grid(row=row, column=column, padx=padx, pady=5, sticky="w")
--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import customtkinter
 from keyboard import keyboard
+import time
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
         self.title("Optimal Keyboard")
-        self.geometry("400x180")
+        self.geometry("600x180")
         self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=0)
 
-        self.keyboard = keyboard(self)
-        self.keyboard.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+    def motherKeyboard(self, characters):
+        if hasattr(self, "keyboardMother"):
+            self.keyboardMother.destroy()
+        self.keyboardMother = keyboard(self, characters)
+        self.keyboardMother.grid(row=0, column=0, padx=10, pady=10, sticky="nsw")
+    
+    def fatherKeyboard(self, characters):
+        if hasattr(self, "keyboardFather"):
+            self.keyboardFather.destroy()
+        self.keyboardFather = keyboard(self, characters)
+        self.keyboardFather.grid(row=0, column=1, padx=10, pady=10, sticky="nse")
 
+alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
+a = ("a " * 26).split()
 app = App()
+app.motherKeyboard(alphabet)
+app.fatherKeyboard(a)
+app.after(5000, lambda : app.motherKeyboard(a))
+app.after(5000, lambda : app.fatherKeyboard(alphabet))
 app.mainloop()
