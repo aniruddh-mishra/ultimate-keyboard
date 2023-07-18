d3768f5b63a90ea9d732d24f2a29e8c983a3aae0
Aniruddh Mishra <aniruddhm17@gmail.com>
2023-07-17 20:25:43 -0500

    stores simulation data

--- display.py
+++ display.py
@@ -38,3 +38,27 @@ class keyboard(customtkinter.CTkFrame):
                 column += 1
             key = customtkinter.CTkLabel(self, text=characters[i], text_color="black")
             key.grid(row=row, column=column, padx=padx, pady=5, sticky="w")
+
+def nextGeneration(genNumber, generations, bestKeyboards, app):
+    if genNumber == generations:
+        return
+    
+    app.motherKeyboard(bestKeyboards[genNumber][0])
+    app.fatherKeyboard(bestKeyboards[genNumber][1])
+    app.after(3000, lambda : nextGeneration(genNumber + 1, generations, bestKeyboards, app))
+
+if __name__ == "__main__":
+    import sys
+    import json
+
+    app = App()
+
+    args = sys.argv[1:]
+    with open(args[0], 'r') as f:
+        data = json.load(f)
+        generations = data['generations']
+        print(f"This simulation had a population size of {data['popSize']} and ran for {generations} generations. It was trained with the text {data['text'][:100]} for the {data['algorithm']} typing style.")
+    bestKeyboards = data['bestKeyboards']
+    nextGeneration(0, generations, bestKeyboards, app)
+
+    app.mainloop()
--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 import display
 import geneticAlgorithm
 import sys
+import json
+from datetime import datetime
+import uuid
 
 def main():
     app = display.App()
 
     if sys.argv[1] in ["-h", "--help"]:
-        print("python3 main.py <population size> <number of generations> <display all generations> <typing style> <training text>\n")
-        print("\t<population size>: number of keyboards in each generation")
-
-        print("\t<number of generations>: number of generations to run the program for")
-        print("\t<display all generations>: 1 to display elites of all generations and 0 to display elites final generation")
-        print("\t<typing style>: rfp for right finger pecking; lfp for left finger pecking; tft for two finger typing; nt (default) for normal ten finger typing")
-        print("\t<training text>: any training text to simulate the fitness test on (default is generic english text)")
-
+        helpDocs()
         return
 
     TEXT, VISUALIZE, ALGORITHM, POP_SIZE, GENERATIONS = setConstants(sys.argv[1:])
 
     bestKeyboards = runSim(GENERATIONS, ALGORITHM, POP_SIZE, TEXT)
 
-    displayKeyboards(VISUALIZE, app, GENERATIONS, bestKeyboards)
+    saveData(bestKeyboards, TEXT, VISUALIZE, ALGORITHM, POP_SIZE, GENERATIONS)
 
+    displayKeyboards(VISUALIZE, app, GENERATIONS, bestKeyboards)
+    
     app.mainloop()
+
+def saveData(bestKeyboards, text, visualize, algorithm, popSize, generations):
+    data = {
+            "text": text,
+            "algorithm": algorithm,
+            "popSize": popSize,
+            "generations": generations,
+            "date": str(datetime.now()),
+            "bestKeyboards": bestKeyboards
+            }
+    jsonData = json.dumps(data)
+
+    with open(f"outputs/{uuid.uuid4()}.json", "w") as f:
+        f.write(jsonData)
 
 def runSim(generations, algorithm, popSize, text):  
     population = geneticAlgorithm.initPopulation(popSize)
@@ -35,6 +47,15 @@ def runSim(generations, algorithm, popSize, text):
         population = geneticAlgorithm.nextPopulation(population, text, algorithm)
 
     return bestKeyboards
+
+def helpDocs():
+    print("python3 main.py <population size> <number of generations> <display all generations> <typing style> <training text>\n")
+    print("\t<population size>: number of keyboards in each generation")
+
+    print("\t<number of generations>: number of generations to run the program for")
+    print("\t<display all generations>: 1 to display elites of all generations and 0 to display elites final generation")
+    print("\t<typing style>: rfp for right finger pecking; lfp for left finger pecking; tft for two finger typing; nt (default) for normal ten finger typing")
+    print("\t<training text>: any training text to simulate the fitness test on (default is generic english text)")
 
 def setConstants(args): 
     POP_SIZE = 10
@@ -62,9 +83,9 @@ def setConstants(args):
 
 def displayKeyboards(visualize, app, generations, parents):
     if visualize:
-        nextGeneration(0, generations, parents, app)
+        display.nextGeneration(0, generations, parents, app)
     else:
-        nextGeneration(generations - 1, generations, parents, app)
+        display.nextGeneration(generations - 1, generations, parents, app)
 
 def updateStatus(progress, generations):
     percent = progress/generations * 100
@@ -72,13 +93,5 @@ def updateStatus(progress, generations):
     complete = "=" * progress
     remainder = " " * (20 - progress)
     print("[" + complete + remainder + "]" + str(round(percent, 2)) + "%\r", end="")
-
-def nextGeneration(genNumber, generations, parents, app):
-    if genNumber == generations:
-        return
-    
-    app.motherKeyboard(parents[genNumber][0])
-    app.fatherKeyboard(parents[genNumber][1])
-    app.after(5000, lambda : nextGeneration(genNumber + 1))
 
 main()
